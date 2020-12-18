a4744ac0f007c27ff654d4ff275854090f802004
MrDrHax <alexplays003@gmail.com>
2020-12-17 20:58:14 -0600

    did a bas thing

--- src/displayapp/screens/Stopwatch.cpp
+++ src/displayapp/screens/Stopwatch.cpp
@@ -10,7 +10,7 @@ extern lv_style_t* LabelBigStyle;
 
 Stopwatch::Stopwatch(Pinetime::Applications::DisplayApp *app) : Screen(app){
     label_time = lv_label_create(lv_scr_act(), nullptr);
-    lv_label_set_style(label_time, LV_LABEL_STYLE_MAIN, LabelBigStyle);
+    //lv_label_set_style(label_time, LV_LABEL_STYLE_MAIN, LabelBigStyle);
     lv_label_set_text(label_time, "TIME");
     lv_obj_set_auto_realign(label_time, true);
     lv_obj_align(label_time, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
