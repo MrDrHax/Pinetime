b550bb8305ad02fe57cfb1c8edadbf65530e20dd
MrDrHax <alexplays003@gmail.com>
2020-12-21 19:53:09 -0600

    trying to add images

--- src/displayapp/screens/Stopwatch.cpp
+++ src/displayapp/screens/Stopwatch.cpp
@@ -1,8 +1,10 @@
 #include "../DisplayApp.h"
 #include "components/datetime/DateTimeController.h"
+#include "displayapp/icons/stopwatch/stopwatch_normal.c"
 #include "Stopwatch.h"
 #include "Symbols.h"
 #include <chrono>
+//#include <cstdio>
 
 using namespace Pinetime::Applications::Screens;
 
@@ -13,6 +15,9 @@ extern lv_font_t jetbrains_mono_extrabold_compressed;
 extern lv_font_t jetbrains_mono_bold_20;
 
 extern lv_style_t* LabelBigStyle;
+ 
+lv_obj_t *img_waitchMain = lv_img_create(lv_scr_act(), NULL);
+
 
 Stopwatch::Stopwatch(DisplayApp* app,
                      Controllers::DateTime& dateTimeController) : Screen(app),
@@ -31,6 +36,10 @@ Stopwatch::Stopwatch(DisplayApp* app,
   lv_label_set_text(label_extra, "");
   lv_obj_align(label_extra, lv_scr_act(), LV_ALIGN_CENTER, 0, label_extra_offset);
   lv_label_set_style(label_extra, LV_LABEL_STYLE_MAIN, LabelBigStyle);
+
+  // set images
+  lv_img_set_src(img_waitchMain, &stopwatch_normal); 
+  lv_obj_align(img_waitchMain, lv_scr_act(), LV_ALIGN_IN_TOP_MID, 0, 0); 
 }
 
 Stopwatch::~Stopwatch() {
@@ -39,8 +48,6 @@ Stopwatch::~Stopwatch() {
 
 bool Stopwatch::Refresh() {
     if (countingTime){
-        // fix this
-
         //1st count new time
 
         int time = getCurrentTime();
